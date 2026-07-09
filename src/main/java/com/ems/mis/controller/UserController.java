13d9cfb76631f6ff31ac123c8126b4c477b5c4cb
kwishaka <sandrineukwishaka6@gmail.com>
2026-07-09 13:45:08 +0300

    Setup Spring Boot with PostgreSQL and implement user registration API

---
+++ src/main/java/com/ems/mis/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.ems.mis.controller;
+
+import com.ems.mis.dto.UserRequestDTO;
+import com.ems.mis.dto.UserResponseDTO;
+import com.ems.mis.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/register")
+    public UserResponseDTO registerUser(@Valid @RequestBody UserRequestDTO dto) {
+        return service.registerUser(dto);
+    }
+
+    @GetMapping
+    public List<UserResponseDTO> getAllUsers() {
+        return service.getAllUsers();
+    }
+}
