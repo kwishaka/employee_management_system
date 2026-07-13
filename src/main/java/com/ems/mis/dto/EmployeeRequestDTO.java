8f297fe5deddaa5af54a37981dcb71a758a14332
kwishaka <sandrineukwishaka6@gmail.com>
2026-07-13 09:14:49 +0300

    Complete Employee CRUD API

--- src/main/java/com/ems/mis/dto/EmployeeRequestDTO.java
+++ src/main/java/com/ems/mis/dto/EmployeeRequestDTO.java
@@ -6,78 +6,82 @@ import jakarta.validation.constraints.NotNull;
 
 public class EmployeeRequestDTO {
 
-    @NotBlank
+    @NotBlank(message = "First name is required")
     private String firstName;
 
-    @NotBlank
+    @NotBlank(message = "Last name is required")
     private String lastName;
 
-    @Email
-    @NotBlank
+    @Email(message = "Invalid email")
+    @NotBlank(message = "Email is required")
     private String email;
 
+    @NotBlank(message = "Phone number is required")
     private String phoneNumber;
 
+    @NotBlank(message = "Department is required")
     private String department;
 
+    @NotBlank(message = "Position is required")
     private String position;
 
-    @NotNull
+    @NotNull(message = "Salary is required")
     private Double salary;
 
-    public EmployeeRequestDTO() {
-    }
+    // ===== GETTERS =====
 
     public String getFirstName() {
         return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
     public String getDepartment() {
         return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
     }
 
     public String getPosition() {
         return position;
     }
 
-    public void setPosition(String position) {
-        this.position = position;
-    }
-
     public Double getSalary() {
         return salary;
+    }
+
+    // ===== SETTERS =====
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
     }
 
     public void setSalary(Double salary) {
--- src/main/java/com/ems/mis/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ems/mis/exception/GlobalExceptionHandler.java
@@ -13,5 +13,9 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return new ResponseEntity<>("Something went wrong.", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
 }
